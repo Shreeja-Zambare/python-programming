86d6b684d1c81e856f449a890c83797ce4c3db83
Shreeja-Zambare <shreejazambare869@gmail.com>
2026-08-19 10:09:40 +0530

    Add list operations program

---
+++ assignment3.py
@@ -0,0 +1,14 @@
+Assignment=["DBMS", "PAI", "Python","Economics"]
+print("\nInitial List : ", Assignment)
+
+#Adding a new element in the list
+Assignment.append("Software Engineering")
+print("\nAfter adding " , Assignment)
+
+#removing a element 
+Assignment.remove("Economics")
+print("\nAfter Removing and element " , Assignment)
+
+print("\nFinal updated list of assignments:",Assignment)
+
+
