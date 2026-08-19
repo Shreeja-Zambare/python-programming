e702627c4d97f4c6d4e6147efed5af3017c21e83
Shreeja-Zambare <shreejazambare869@gmail.com>
2026-08-19 10:05:10 +0530

    Add Earth and Moon weight program

---
+++ Weigth.py
@@ -0,0 +1,5 @@
+mass = float(input("Enter the mass of the object (in kg): "))
+
+print(f"\nMass of the object: {mass} kg")
+print(f"Weight on Earth: {mass*9.81} N")
+print(f"Weight on Moon: {mass*1.62} N")
